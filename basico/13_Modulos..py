7302b158f1cc39316ece3b27a8ac42abe9422331
Marlon Gonzalez Veloz <marlonglezveloz@gmail.com>
2023-07-03 17:50:03 -0500

    curso intermedio

---
+++ basico/13_Modulos..py
@@ -0,0 +1,7 @@
+## Modules ##
+import module #importra el modulo completo
+from module import sum_two_values # importar una funcion en especifico
+sum_two_values(2,3)
+module.sum_two_values(2,3)
+
+from math import pi as Pi_value # importando pi en una variable
---
+++ basico/module.py
@@ -0,0 +1,2 @@
+def sum_two_values(first_number, second_number):
+    print(first_number + second_number)
