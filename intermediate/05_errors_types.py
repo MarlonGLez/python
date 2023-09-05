9a6ef9cb2c1177f9812dbaa3162defa88de846d0
MarlonGLez <126759911+MarlonGLez@users.noreply.github.com>
2023-09-05 17:24:02 -0500

    file handing

---
+++ intermediate/05_errors_types.py
@@ -0,0 +1,22 @@
+### Error Types ###
+
+# SyntaxErro
+#print "Hola Comunidad"# error
+
+# NameError
+#print(language)  #Descomentar para Error
+
+# IndexError
+my_list = ["Python", "Swift", "Kotlin","dart", "JAvaScrip"]
+print(my_list[0])
+print(my_list[-1])
+print(my_list[4])
+#print(my_list[5]) #Descomentar para Error
+ 
+#Modulenotfound Error
+#import maths #Descomentar para Error
+import math
+
+# AttributeError
+#print(math.pI) #Descomentar para Error
+print(math.pi)
