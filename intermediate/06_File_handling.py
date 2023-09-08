788bd1e5b330061be7cbcb52d923d1b13191e12a
MarlonGLez <126759911+MarlonGLez@users.noreply.github.com>
2023-09-08 10:45:26 -0500

    file handling

--- intermediate/06_File_handling.py
+++ intermediate/06_File_handling.py
@@ -1,5 +1,11 @@
 ### File Handling ###
 import os
 txt_file= open("intermediate/my_file.txt", "r+") 
+#print(txt_file.read())
+#print(txt_file.read(10)) # lee cantidad de caracteres
+#print(txt_file.readline()) # line a linea
+#print(txt_file.readlines()) #cada linea lo convierte en un elemento de una lista
+for line in txt_file.readlines():
+    print(line)
 
-print(txt_file.read(10))
+txt_file.write("\nAunque tambin kotlin")
